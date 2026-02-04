c6a0a6c0163035c99a9def1b2590cb76cdd31c19
shivansh <trivedishivansh1207@gmail.com>
2026-02-05 00:33:13 +0530

    for deployment

--- backend/main.go
+++ backend/main.go
@@ -14,10 +14,10 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error loading env")
-	}
+	_ = godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("error loading env")
+	// }
 
 	dbURL := os.Getenv("DB_URL")
 	db, err := sql.Open("postgres", dbURL)
